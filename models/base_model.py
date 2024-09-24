5852edcb07275d59ef0684f89ed95d6cb117e2a0
Sng43 <daemmyoff1cial@gmail.com>
2024-09-24 15:10:06 +0200

    orm relationships

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=Fals)
+    created_at = Column(DateTime, nullable=False, default =datetime.now())
+    updated_at = Column(DateTime, nullable=False, default =datetime.now())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if kwargs:
@@ -18,11 +25,9 @@ class BaseModel:
 
                     setattr(self, k, v)
         else:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
 
     def __str__(self):
@@ -34,6 +39,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,4 +49,13 @@ class BaseModel:
         to_json['__class__'] = self.__class__.__name__
         to_json['created_at'] = self.created_at.isoformat()
         to_json['updated_at'] = self.updated_at.isoformat()
+        to_json.pop('_sa_instance_state')
         return to_json
+    
+    def delete(self):
+        """
+        
+        """
+
+        from models import storage
+        storage.delete(self)
