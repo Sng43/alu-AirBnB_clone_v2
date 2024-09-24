8bd58796cb9d3e46a8d68b6c3e43748d5c6b9003
Sng43 <daemmyoff1cial@gmail.com>
2024-09-25 01:56:50 +0200

    debugging the sb_storage

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,11 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default =datetime.now())
-    updated_at = Column(DateTime, nullable=False, default =datetime.now())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow) 
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if kwargs:
             for k, v in kwargs.items():
                 if k == '__class__':
@@ -23,25 +23,23 @@ class BaseModel:
                 if k in ['created_at', 'updated_at'] and isinstance(v, str):
                     v = datetime.fromisoformat(v)
 
-                    setattr(self, k, v)
+                setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
+            self.created_at = datetime.utcnow() 
+            self.updated_at = datetime.utcnow()  
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        # cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow() 
 
-        storage.new(self)
-        storage.save()
+        storage.new(self)  
+        storage.save()  
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -49,13 +47,10 @@ class BaseModel:
         to_json['__class__'] = self.__class__.__name__
         to_json['created_at'] = self.created_at.isoformat()
         to_json['updated_at'] = self.updated_at.isoformat()
-        to_json.pop('_sa_instance_state')
+        to_json.pop('_sa_instance_state', None) 
         return to_json
     
     def delete(self):
-        """
-        
-        """
-
+        """Delete the current instance from storage"""
         from models import storage
         storage.delete(self)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-""" 
-
-"""
+"""Database storage for the HBNB project."""
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from models.base_model import Base, BaseModel
+from models.base_model import Base
 from models.state import State
 from models.city import City
 from models.place import Place
@@ -14,75 +12,64 @@ from models.review import Review
 from models.amenity import Amenity
 
 class DBStorage:
-    """
+    """Database storage engine for the HBNB project."""
     
-    """
-
     __engine = None
     __session = None
 
     def __init__(self):
-        """
-        
-        """
-
+        """Initialize the database storage engine."""
         username = getenv('HBNB_MYSQL_USER')
         password = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
         db_name = getenv('HBNB_MYSQL_DB')
 
-        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(username, password, host, db_name)
-
+        db_url = f"mysql+mysqldb://{username}:{password}@{host}/{db_name}"
         self.__engine = create_engine(db_url, pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
-    
     def all(self, cls=None):
-        """
-        
-        """
+        """Query all objects of the given class or all classes."""
         objs_list = []
         if cls:
             if isinstance(cls, str):
-                try:
-                    cls =globals()[cls]
-                except KeyError:
-                    pass
+                cls = globals().get(cls)
             if issubclass(cls, Base):
                 objs_list = self.__session.query(cls).all()
             else:
                 for subclass in Base.__subclasses__():
                     objs_list.extend(self.__session.query(subclass).all())
-            obj_dict = {}
-            for obj in objs_list:
-                key = "{}.{}".format(obj.__class__.__name__, obj.id)
-                try:
-                    del obj._sa_instance_state
-                    obj_dict[key] = obj
-                except Exception:
-                    pass
-            return obj_dict
+        else:
+            objs_list = self.__session.query(State).all() + self.__session.query(City).all()  # Include other classes as needed
+
+        obj_dict = {}
+        for obj in objs_list:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            obj_dict[key] = obj
+            del obj._sa_instance_state 
+        return obj_dict
         
     def new(self, obj):
-        "add the object to the current database session"
+        """Add a new object to the current database session."""
         self.__session.add(obj)
     
-    def save(self, obj):
-        "commit all changes to the current database session"
+    def save(self):
+        """Commit all changes to the current database session."""
         self.__session.commit()
     
     def delete(self, obj=None):
-        "delete from current database session"
-        self.__session.delete(obj)
-
+        """Delete an object from the current database session."""
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
-        """
-        
-        """
+        """Create all tables in the database and create a new session."""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(session_factory)
+
+    def close(self):
+        """Remove the current session."""
+        self.__session.remove()
