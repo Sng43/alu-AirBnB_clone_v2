9b1868bf19bbfd5ef87ca78d1e0a0c22ae6bc9f8
Sng43 <daemmyoff1cial@gmail.com>
2024-09-25 03:09:07 +0200

    review

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from os import getenv
-from sqlalchemy import (Column,String, ForeignKey, Float, Integer)
+from sqlalchemy import Column, String, ForeignKey, Float, Integer
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 import models
@@ -11,26 +11,28 @@ from models.review import Review
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer,default = 0)
-    number_bathrooms = Column(Integer,default = 0)
-    max_guest = Column(Integer,default = 0)
-    price_by_night = Column(Integer,default = 0)
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship('Review', backref='place', cascade='all, delete-orphan')
-    amenity_ids = []
 
+    # Using string reference for relationships
+    reviews = relationship('Review', backref='place', cascade='all, delete-orphan')
+
+    # For storage types other than db
+    amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def reviews(self):
-            """
-            
-            """
+            """ Return the list of reviews associated with the place. """
             all_reviews = list(models.storage.all(Review).values())
-            review_list = [r for r in all_reviews if r.plcae_id == self.id]
+            review_list = [r for r in all_reviews if r.place_id == self.id]  # Fixed typo here
             return review_list
