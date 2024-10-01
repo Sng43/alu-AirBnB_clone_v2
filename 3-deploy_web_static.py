561dc16a0b2f16daf073cc3b6a8cbda481ae69ea
Your Name <daemmyoff1cial@gmail.com>
2024-10-01 08:55:44 +0000

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""
+a Fabric script (based on the file 2-do_deploy_web_static.py) that creates 
+and distributes an archive to your web servers
+
+"""
 import os.path
 from fabric.api import env
 from fabric.api import put
