77797d87479f589563fa2612837c48710b556009
Sng43 <daemmyoff1cial@gmail.com>
2024-10-01 14:14:15 +0200

    fad?

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-"""
-A Fabric script that creates and distributes an archive to your web servers.
-
-This script allows you to deploy a web application archive to specified web servers.
-It checks if the archive exists, uploads it to the server, and extracts it into the correct directory.
-"""
-
+# Fabfile to distribute an archive to a web server.
 import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-# Define the host servers for deployment
 env.hosts = ["3.88.187.103", "3.87.94.49"]
+
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
 
-    This function takes the path of an archive file, checks if it exists,
-    uploads it to a remote server, and extracts it to the appropriate location.
-
     Args:
         archive_path (str): The path of the archive to distribute.
-
     Returns:
-        bool: 
-            - False if the file doesn't exist at `archive_path` or if an error occurs during the process.
-            - True if the deployment is successful.
-
-    Steps:
-        1. Check if the archive exists at the specified `archive_path`.
-        2. Extract the filename and name (without extension) from the archive path.
-        3. Upload the archive to the remote server's temporary directory.
-        4. Remove any existing release folder for the application on the server.
-        5. Create a new directory for the application release.
-        6. Extract the uploaded archive into the new release directory.
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
-    
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
+
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
