24ed6208ba26e187068c9f70842111b0535d1e3e
terrymanzi <m.terry@alustudent.com>
2024-10-05 18:44:00 +0000

    task0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,47 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
+# Update package lists and install Nginx
+sudo apt-get update
+sudo apt-get install -y nginx
+
+# Create necessary directories for web_static
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a sample index.html file for testing
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+# Update the Nginx configuration
+sudo printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
+
+# Restart the Nginx service to apply the changes
+sudo service nginx restart
