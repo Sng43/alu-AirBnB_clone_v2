99042f3fa9fe66d8e19ea38c7189ca0148b2892c
Sng43 <daemmyoff1cial@gmail.com>
2024-10-08 10:23:56 +0200

    is even maybe odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,5 +50,18 @@ def number_template(n):
     """
     return render_template('5-number.html', number=n)
 
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """
+    
+    """
+    even_odd = "even" if n % 2 == 0 else "odd"
+    values = {
+        "number": n,
+        "even_or_odd": even_odd
+    }
+    return render_template('6-number_odd_or_even.html', values=values)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
