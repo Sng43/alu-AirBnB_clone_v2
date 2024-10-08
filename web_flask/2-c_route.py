69bc5e303afed023dbd61c7cb31aba216066128f
Sng43 <daemmyoff1cial@gmail.com>
2024-10-08 09:14:48 +0200

    hbnb parameters

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,18 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
-    """
-    
-    """
+    """ Home route """
     return 'hello HBNB'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    
-    """
+    """ HBNB route """
     return 'HBNB'
-    
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ c route with dynamic text """
+    text_with_no_slashes = text.replace('_', ' ')
+    return f"C {text_with_no_slashes}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
